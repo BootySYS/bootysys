f7ac942613c27284905ea334d446105f9d1e612f
Enes Kaya <eneskaya@me.com>
2015-10-20 20:27:31 +0200

    New test case

---
+++ tests/SignUpTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SignUpTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Test the sign up page.
+     *
+     * @return void
+     */
+    public function testSignUpPage()
+    {
+        $this->visit('/register/university')
+             ->see('Register now!')
+             ->type('HAW Hamburg', 'name')
+             ->type('Hans', 'contact_first_name')
+             ->type('Müller', 'contact_last_name')
+             ->type('hans.mueller@gmail.com', 'email')
+             ->type('Hamburg', 'state')
+             ->type('Hamburg', 'city')
+             ->type('20095', 'zip_code')
+             ->type('Berliner Tor 5', 'street')
+             ->type('1234', 'password')
+             ->type('1234', 'confirm_password')
+             ->press('Register')
+             ->seePageIs('/dashboard');
+    }
+
+    public function testLogoutAndLogin()
+    {
+        $this->visit('/auth/login')
+             ->type('enes.kaya@haw-hamburg.de', 'email')
+             ->type('1234', 'password')
+             ->press('Log in')
+             ->seePageIs('/dashboard');
+
+        $this->visit('/dashboard')
+             ->click('Log out')
+             ->seePageIs('/');
+    }
+}
