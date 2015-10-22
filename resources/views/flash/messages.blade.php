5763a96bc3da38405d7002e7167c8cfbd9efd686
Enes Kaya <eneskaya@me.com>
2015-10-22 17:58:35 +0200

    Prettier flash messages

--- resources/views/flash/messages.blade.php
+++ resources/views/flash/messages.blade.php
@@ -1,5 +1,7 @@
 @if (count($errors) > 0)
     <div class="alert alert-danger errors">
+        <p><strong>Whoops, looks like something went wrong.</strong></p>
+        <br>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
